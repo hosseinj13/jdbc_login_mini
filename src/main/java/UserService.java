77789c1436443748cadeb03f32185f019cf60742
hossein javadi <hosseinj13@yahoo.com>
2024-01-08 23:58:14 +0330

    add signup menu

--- src/main/java/UserService.java
+++ src/main/java/UserService.java
@@ -1,2 +1,32 @@
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class UserService {
+    private final Scanner scanner = new Scanner(System.in);
+    private final UserRepository userRepository = new UserRepository();
+
+    public UserService() throws SQLException {
+    }
+
+    public void signup() throws SQLException {
+
+        System.out.println("Please enter your firstname:  ");
+        String firstname = scanner.nextLine();
+
+        System.out.println("Please enter your lastname:  ");
+        String lastname = scanner.nextLine();
+
+        System.out.println("Please enter your username:  ");
+        String username = scanner.nextLine();
+
+        System.out.println("Please enter your password:  ");
+        String password = scanner.nextLine();
+
+        User user = new User(firstname, lastname, username, password);
+        int result = userRepository.registerUser(user);
+        if (result == 1)
+            System.out.println(firstname + " successfully register :)");
+        else
+            System.out.println("somethings is wrong :(");
+    }
 }

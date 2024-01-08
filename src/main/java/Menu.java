bc12c586729949cbaf1c6802359dd1b573f05bee
hossein javadi <hosseinj13@yahoo.com>
2024-01-08 17:46:00 +0330

    Create publicMenu in Menu class

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Menu {
+    Scanner scanner = new Scanner(System.in);
+    public void publicMenu(){
+        System.out.println("****** WELCOME ******");
+        System.out.println("1-SIGN UP");
+        System.out.println("2-SIGN IN");
+        System.out.println("3-EXIT");
+        System.out.println("Choose your number: ");
+
+        int number = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println(number);
+    }
 }

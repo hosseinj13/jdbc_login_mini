18d6cfd94bcf5fe3402da2b19bec889bab041cc2
hossein javadi <hosseinj13@yahoo.com>
2024-01-08 17:21:28 +0330

    Add no args and all args costructor

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -3,4 +3,14 @@ public class User {
     private String lastNAme;
     private String username;
     private String password;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastNAme, String username, String password) {
+        this.firstName = firstName;
+        this.lastNAme = lastNAme;
+        this.username = username;
+        this.password = password;
+    }
 }
